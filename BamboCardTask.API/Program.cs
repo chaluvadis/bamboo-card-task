31d8e6d26c76c5ce4f8ecde354e1b512757acca7
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2025-05-12 23:14:54 +0400

    response cache middleware added

--- BamboCardTask.API/Program.cs
+++ BamboCardTask.API/Program.cs
@@ -3,6 +3,7 @@ using BambooCardTask.Interfaces;
 using BambooCardTask.Models;
 using BambooCardTask.Routes;
 using BambooCardTask.Services;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +28,8 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.WriteIndented = true;
 });
 
+builder.Services.AddResponseCaching();
+
 // Register ExchangeRateService as a service
 builder.Services.AddScoped<IExchangeRateService, ExchangeRateService>();
 
@@ -39,6 +42,22 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseResponseCaching();
+
+app.Use(async (context, next) =>
+{
+    context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
+    {
+        Public = true,
+        MaxAge = TimeSpan.FromSeconds(60)
+    };
+
+    // Add Vary header for the User-Agent
+    context.Response.Headers[HeaderNames.Vary] = "User-Agent";
+
+    await next();
+});
 
 app.UseHttpsRedirection();
 
