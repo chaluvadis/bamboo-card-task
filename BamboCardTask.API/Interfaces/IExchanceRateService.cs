68ac838ec105bba2b611ddf4c8df49152363c80e
Surendra Chaluvadi <surendra.chaluvadi@gmail.com>
2025-05-14 12:32:19 +0400

    updated to handle null response

--- BamboCardTask.API/Interfaces/IExchanceRateService.cs
+++ BamboCardTask.API/Interfaces/IExchanceRateService.cs
@@ -1,8 +1,7 @@
-using BambooCardTask.Models;
 namespace BambooCardTask.Interfaces;
 public interface IExchangeRateService
 {
     ValueTask<ExchangeRatesResponse?> GetLatestExchangeRatesAsync(string baseCurrency);
     ValueTask<ConversionRatesResponse?> GetConversionRatesAsync(CurrencyConversionRequest currencyConversionRequest);
-    ValueTask<HistoricalExchangeRatesResponse> GetHistoricalExchangeRatesAsync(HistoricalExchangeRatesRequest historicalExchangeRates);
+    ValueTask<HistoricalExchangeRatesResponse?> GetHistoricalExchangeRatesAsync(HistoricalExchangeRatesRequest historicalExchangeRates);
 }
